f1435586331b943199f93d04dab2cbefefb4f929
StReiF7493 <61586336+StReiF7493@users.noreply.github.com>
2020-03-31 13:44:36 +0300

    dz

--- Lesson2/Calculator.java
+++ Lesson2/Calculator.java
@@ -4,37 +4,6 @@ public class Calculator {
         private char operation;
         private int number2;
         private int result = 1;
-        private String request1 = "Введите первое число: ";
-        private String request2 = "Введите знак математической операции: ";
-        private String request3 = "Введите второе число: ";
-
-        public String getRequest1() {
-            return request1;
-        }
-
-        void setRequest1(String request1) {
-            this.request1 = request1;
-        }
-
-        public String getRequest2() {
-            return request2;
-        }
-
-        void setReuqest2(String request2) {
-            this.request2 = request2;
-        }
-
-        public String getRequest3() {
-            return request3;
-        }
-
-        void setRequest3(String request3) {
-            this.request3 = request3;
-        }
-
-        public int getNumber1() {
-            return number1;
-        }
 
         void setNumber1(int number1) {
             this.number1 = number1;
@@ -64,7 +33,7 @@ public class Calculator {
             this.result = result;
         }
 
-        public void calc() {
+        public void calculate() {
             switch(operation) {
                 case '+':
                     result = number1 + number2;
@@ -80,10 +49,7 @@ public class Calculator {
                     break;
                 case '^':
                     for(int i = 1; i <= number2; i++) {
-                    result *= number1;
-                    }
-                    if(number2 == 0) {
-                        System.out.println(1);
+                        result *= number1;
                     }
                     break;
             }
--- Lesson2/CalculatorTest.java
+++ Lesson2/CalculatorTest.java
@@ -3,18 +3,32 @@ public class CalculatorTest {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         Calculator calculatorOne = new Calculator();
-        System.out.println(calculatorOne.getRequest1());
+
+        System.out.println("Введите первое число: ");
         calculatorOne.setNumber1(scan.nextInt());
-        System.out.println(calculatorOne.getRequest2());
+        System.out.println("Введите знак математической операции: ");
         calculatorOne.setOperation(scan.next().charAt(0));
-        System.out.println(calculatorOne.getRequest3());
+        System.out.println("Введите второе число: ");
         calculatorOne.setNumber2(scan.nextInt());
         calculatorOne.getResult();
-        calculatorOne.calc();
-        System.out.println("Сумма математической операции: " + calculatorOne.getResult());
-
+        calculatorOne.calculate();
+        System.out.println("Результат математической операции: " + calculatorOne.getResult());
+        String request = "Хотите продолжить?[да/нет] ";
+        boolean exit = false;
+        for (int i = 1; ;) {
+            if(exit == true) {
+            break;
+        }
+            System.out.println(request);
+            String replay = scan.next();
+            switch(replay) {
+                case "да":
+                    main(args);
+                    break;
+                case "нет":
+                    exit = true;
+                    break;
+            }
+        }
     }
 }
-
-
-
