c7d139d018ff3441bd1a21bc0d0d2f454ce9c276
StReiF7493 <61586336+StReiF7493@users.noreply.github.com>
2020-04-07 15:33:53 +0300

    dz

--- Lesson2/конструктор/Jaeger.java
+++ Lesson2/конструктор/Jaeger.java
@@ -1,23 +1,58 @@
 public class Jaeger {
 
-    String modelName;
-    String mark;
-    String origin;
-    float height;
-    float weight;
-    int speed;
-    int strength;
-    int armor;
+    private String modelName;
+    private String mark;
+    private String origin;
+    private float height;
+    private float weight;
+    private int speed;
+    private int strength;
+    private int armor;
 
-    public Jaeger(String modelName, String mark, String origin, float height, float weight, int speed, int strength, int armor) {
-        this.modelName = modelName;
-        this.mark = mark;
-        this.origin = origin;
-        this.height = height;
-        this.weight = weight;
-        this.speed = speed;
-        this.strength = strength;
-        this.armor = armor;
+    public String getModelName() {
+        return modelName;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public int getArmor() {
+        return armor;
+    }
+
+
+
+    public Jaeger(String modelName2, String mark2, String origin2, float height2, float weight2, int speed2, int strength2, int armor2) {
+
+        modelName = modelName2;
+        mark = mark2;
+        origin = origin2;
+        height = height2;
+        weight = weight2;
+        speed = speed2;
+        strength = strength2;
+        armor = armor2;
     }
 
     public void palmGripBacklash() {
