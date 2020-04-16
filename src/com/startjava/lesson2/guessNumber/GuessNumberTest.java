f9350dd5f6ab078b3554edf8b01852120c571a42
StReiF7493 <61586336+StReiF7493@users.noreply.github.com>
2020-04-16 15:27:18 +0300

    dz

---
+++ src/com/startjava/lesson2/guessNumber/GuessNumberTest.java
@@ -0,0 +1,25 @@
+package com.startjava.lesson2.guessNumber;
+
+import java.util.Scanner;
+
+public class GuessNumberTest {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите имя первого игрока");
+        Player playerOne = new Player(scan.next());
+        System.out.println("Введите имя второго игрока");
+        Player playerTwo = new Player(scan.next());
+        GuessNumber game = new GuessNumber(playerOne, playerTwo);
+        game.startGame();
+        String replay;
+        do {
+            System.out.println("Хотите продолжить?[да/нет]");
+            replay = scan.next();
+            if(replay.equals("да")) {
+                main(args);
+                break;
+            }
+        } while(!replay.equals("нет"));
+    }
+
+}
---
+++ src/com/startjava/lesson2/guessNumber/Player.java
@@ -0,0 +1,25 @@
+package com.startjava.lesson2.guessNumber;
+
+import java.util.Scanner;
+
+public class Player {
+    Scanner scan = new Scanner(System.in);
+    private String name;
+    private int number;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    void setNumber(int number) {
+        this.number = number;
+    }
+}
