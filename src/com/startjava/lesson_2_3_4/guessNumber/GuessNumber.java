3dabf8030da42a2ffcff708f571c2e76e0af8a94
StReiF7493 <61586336+StReiF7493@users.noreply.github.com>
2020-05-23 22:10:57 +0300

    Update GuessNumber.java

--- src/com/startjava/lesson_2_3_4/guessNumber/GuessNumber.java
+++ src/com/startjava/lesson_2_3_4/guessNumber/GuessNumber.java
@@ -6,7 +6,6 @@ import java.util.Scanner;
 public class GuessNumber {
     Scanner scan = new Scanner(System.in);
     private int compNumber;
-    private boolean victory;
     private Player playerOne;
     private Player playerTwo;
 
@@ -17,19 +16,21 @@ public class GuessNumber {
 
     public void startGame() {
         compNumber = (int) (Math.random() * 101);
-        while (playerTwo.getAttempts() < 10 & victory == false) {
+        while (playerTwo.getAttempts() < 10) {
             inputNumber(playerOne);
-            compareNums(playerOne);
-            if(victory == true) {
+            if(compareNums(playerOne) == true) {
                 break;
             }
             inputNumber(playerTwo);
-            compareNums(playerTwo);
+            if(compareNums(playerTwo) == true) {
+                break;
+            }
         }
-        if (playerTwo.getAttempts() > 9 & playerOne.getAttempts() > 9 ) {
-            loss(playerOne);
-            loss(playerTwo);
+        if (playerTwo.getAttempts() > 9 & playerOne.getAttempts() > 9) {
+            System.out.println("У всех игроков закончились попытки.");
         }
+        System.out.println("Числа введённые игроком " + playerOne.getName() + ": " + Arrays.toString(playerOne.getNumbers()));
+        System.out.println("Числа введённые игроком " + playerTwo.getName() + ": " + Arrays.toString(playerTwo.getNumbers()));
         playerOne.zeroing();
         playerTwo.zeroing();
     }
@@ -39,21 +40,16 @@ public class GuessNumber {
         player.setNumber(scan.nextInt());
     }
 
-    private void compareNums(Player player) {
-        if (player.getNumber() > compNumber) {
+    private boolean compareNums(Player player) {
+        if (player.getNumber() == compNumber) {
+            System.out.println("Игрок " + player.getName() + " угадал число " + compNumber + " с " + player.getAttempts() + " попытки");
+            return true;
+        }
+        else if (player.getNumber() > compNumber) {
             System.out.println("Введенное вами число больше того, что загадал компьютер");
         } else if (player.getNumber() < compNumber) {
             System.out.println("Введенное вами число меньше того, что загадал компьютер");
-        } else if (player.getNumber() == compNumber) {
-            victory = (true);
-            System.out.println("Игрок " + player.getName() + " угадал число " + compNumber + " с " + player.getAttempts() + " попытки");
-            System.out.println("Числа введённые игроком " + playerOne.getName() + ": " + Arrays.toString(playerOne.getNumbers()));
-            System.out.println("Числа введённые игроком " + playerTwo.getName() + ": " + Arrays.toString(playerTwo.getNumbers()));
         }
-    }
-
-    private void loss(Player player) {
-        System.out.println("У игрока " + player.getName() + " закончились попытки.");
-        System.out.println("Числа введённые игроком " + player.getName() + ": " + Arrays.toString(player.getNumbers()));
+        return false;
     }
 }
